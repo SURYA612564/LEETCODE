b87d3428cdf02bc0ccfbaea3c2b5dc20c91bc4d2
surya <suryasiet1419@gmail.com>
2026-06-22 09:06:04 +0530

    Second

---
+++ PalindromeNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        int y=x;
+        int rev=0;
+        while(x!=0)
+        {
+            int rem=x%10;
+            rev=(rev*10)+rem;
+            x=x/10;
+        }
+        if(rev==y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
