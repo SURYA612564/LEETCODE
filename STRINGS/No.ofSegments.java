7e3a54d03be36db99f894c6860129513f68d7418
surya <suryasiet1419@gmail.com>
2026-06-29 21:07:22 +0530

    Today's Prblm

---
+++ STRINGS/No.ofSegments.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int countSegments(String s) {
+        s=s.trim().replaceAll("\\s+"," ");
+        if(s.equals(""))return 0;
+        String[]parts=s.split(" ");
+        return parts.length;
+    }
+}
