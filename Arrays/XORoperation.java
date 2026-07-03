1367faeb229788f91c04607556540fa194991104
surya <suryasiet1419@gmail.com>
2026-07-03 10:20:26 +0530

    03rd JULY

---
+++ Arrays/XORoperation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int arr[]=new int[n];
+        arr[0]=start;
+        for(int i=1;i<n;i++)
+        {
+            arr[i]=start+2*i;
+        }
+        int x=0;
+        for(int i=0;i<n;i++)
+        {
+            x=x^arr[i];
+        }
+        return x;
+    }
+}
