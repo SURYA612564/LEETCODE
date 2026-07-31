e64a91cba7b5bf797134449859a97f0ef1729911
surya <suryasiet1419@gmail.com>
2026-07-31 11:58:35 +0530

    31th JULY

---
+++ MATH/CountSquare.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countTriples(int n) {
+        int c=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=2;j<=n;j++)
+            {
+                for(int k=3;k<=n;k++)
+                {
+                    if((i*i)+(j*j)==(k*k))c++;
+                }
+            }
+        }
+        return c;
+
+    }
+}
---
+++ MATH/MaxContainers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxContainers(int n, int w, int maxWeight) {
+        int c=0;
+        for(int i=1;i<=n*n;i++)
+        {
+            if(i*w<=maxWeight)c++;
+        }
+        return c;
+
+    }
+}
