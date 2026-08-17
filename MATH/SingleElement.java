e3eb8823429bbf546f0b746084cc9fbfc005fdc0
surya <suryasiet1419@gmail.com>
2026-08-17 08:41:05 +0530

    17th AUG

---
+++ MATH/SingleElement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int x : nums)
+        {
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        for(int key : map.keySet())
+        {
+            if(map.get(key)==1)
+            {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
