1757b620e7e078c52737f8a4044baf3005de34c0
surya <suryasiet1419@gmail.com>
2026-09-18 08:37:06 +0530

    Delete and Earn problem

---
+++ DYNAMIC PROGRAMMING/DeleteandEarn.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int n=nums.length;
+        int max=0;
+        for(int x : nums)
+        {
+            max=Math.max(max,x);
+        }
+        int points[]=new int[max+1];
+        for(int x : nums)
+        {
+            points[x]+=x;
+        }
+        int dp[]=new int[max+1];
+        dp[0]=0;
+        if(max>=1)
+        {
+            dp[1]=points[1];
+        }
+        for(int i=2;i<=max;i++)
+        {
+            dp[i]=Math.max(dp[i-1],dp[i-2]+points[i]);
+        }
+        return dp[max];
+    }
+}
